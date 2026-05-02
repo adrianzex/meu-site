64353bb0dfc08ceb1722cdc0c5dc9d2437a17b82
adrian <adrianribeiro837@gmail.com>
2026-05-02 16:11:59 -0300

    remove categoria vazia

--- products.ts
+++ products.ts
@@ -79,4 +79,4 @@ export const products: Product[] = [
   },
 ];
 
-export const categories = ["Corset", "Sutiãs", "Conjuntos", "Espartilhos", "Camisolas"];
+export const categories = ["Corset", "Sutiãs", "Conjuntos", "Espartilhos", "Cropped"];
